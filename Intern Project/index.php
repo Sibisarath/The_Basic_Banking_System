c6d83f39a622a3064dde974351760a2c88d5987e
Sibisarath <60036932+Sibisarath@users.noreply.github.com>
2021-06-13 12:11:31 +0530

    Add files via upload

---
+++ Intern Project/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Welcome To GRIP Bank </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="style1.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css">
+
+    </head>
+    <?php
+    
+    ?>
+    <body>
+        <div class="hero">
+            <div class="nav-bar">
+                <div class="nav-logo">
+                    <img src="logo.png">
+        </div>
+        <div class="nav-links">
+            <ul>
+                <a href="index.php"><li>HOME</li></a>
+                <a href="transfermoney.php"><li>TRANSFER MONEY</li></a>
+                <a href="transactionhistory.php"><li>TRANSACTION HISTORY</li></a>
+                <a href="createuser.php"><li>CREATE USER</li></a>
+
+            </ul>
+        </div>
+            </div>
+            <div class="heading-title">
+                <center><p class="p1">The Sparks Bank</p></center>
+            </div>
+
+        <div class="banner-title">
+            <h1>Your<span> Perfect<br>Banking</span> Partner</h1>
+        </div>
+        <div class="banner-btn">
+            <button class="btn" onclick="window.location.href='createuser.php'">CREATE USER</button>
+
+        </div>
+        
+    </body>
+</html>
